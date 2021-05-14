7fce25ee7994a9e57b14381ebe0a52b935a988c1
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-14 13:56:24 +0530

    compare length of two lines

--- GeometryLineProblem.java
+++ GeometryLineProblem.java
@@ -8,18 +8,42 @@ public class GeometryLineProblem {
 	Scanner n=new Scanner(System.in);
 	
 	System.out.println("Enter values of point X1");
-	int X1= n.nextInt();
+	int line1X1= n.nextInt();
 	System.out.println("Enter values of point Y1");
-	int Y1=n.nextInt();
+	int line1Y1=n.nextInt();
 	System.out.println("Enter values of point X2");
-	int X2=n.nextInt();
+	int line1X2=n.nextInt();
 	System.out.println("Enter values of point Y2");
-	int Y2=n.nextInt();
-	System.out.println("Points of line1 are X="+"("+X1+","+X2+")"+" Y="+"("+Y1+","+Y2+")");
+	int line1Y2=n.nextInt();
+	System.out.println("Points of line1 are X="+"("+line1X1+","+line1X2+")"+" Y="+"("+line1Y1+","+line1Y2+")");
 
-	double length= Math.sqrt((X2-X1)^2+(Y2-Y1)^2);
-	 System.out.println("Length of is "+ length);
+	double lengthline1= Math.sqrt((line1X2-line1X1)^2+(line1Y2-line1Y1)^2);
+	 System.out.println("length of line1 is "+ lengthline1);
+	 
+	 System.out.println("Enter values of point X1");
+	 int line2X1=n.nextInt();
+	 System.out.println("Enter values of point Y1");
+	 int line2Y1=n.nextInt();
+	 System.out.println("Enter values of point X2");
+	 int line2X2=n.nextInt();
+	 System.out.println("Enter values of point Y2");
+	 int line2Y2=n.nextInt();
+	 System.out.println("Points of line2 are X="+"("+line2X1+","+line2X2+")"+" Y="+"("+line2Y1+","+line2Y2+")");
+
+	 double lengthline2=Math.sqrt((line2X2-line2X1)^2+(line2Y2-line2Y1)^2);
+	 System.out.println("length of line2 is "+lengthline2);
+	 
+	 if(lengthline1==lengthline2) {
+		 System.out.println("Both lines are Equal");
+	 }
+	 else {
+		 System.out.println("lines are Not Equal");
+	 }
+		 
+}
+
+	 
 	 
 }
-}
+
 
