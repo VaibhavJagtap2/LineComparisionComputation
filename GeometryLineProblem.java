fe38f81401af71590ea77f145b33eb8501aedb1d
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-14 13:41:46 +0530

    add length of line

--- GeometryLineProblem.java
+++ GeometryLineProblem.java
@@ -1,9 +1,25 @@
 package com.geometrylineproblem;
 
-public class GeometryLineProblem {
-public static void main(String[] args) {
-		
-	    System.out.println("Welcome To The Line Comparision Computation");
+import java.util.Scanner;
 
+public class GeometryLineProblem {
+	public static void main(String[] args) {
+		
+	Scanner n=new Scanner(System.in);
+	
+	System.out.println("Enter values of point X1");
+	int X1= n.nextInt();
+	System.out.println("Enter values of point Y1");
+	int Y1=n.nextInt();
+	System.out.println("Enter values of point X2");
+	int X2=n.nextInt();
+	System.out.println("Enter values of point Y2");
+	int Y2=n.nextInt();
+	System.out.println("Points of line1 are X="+"("+X1+","+X2+")"+" Y="+"("+Y1+","+Y2+")");
+
+	double length= Math.sqrt((X2-X1)^2+(Y2-Y1)^2);
+	 System.out.println("Length of is "+ length);
+	 
 }
 }
+
