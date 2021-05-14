155d4be6c8cb960fab89d14d90541dd23d8e8fa1
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-14 13:36:25 +0530

    add welcome to line Computation

---
+++ GeometryLineProblem.java
@@ -0,0 +1,9 @@
+package com.geometrylineproblem;
+
+public class GeometryLineProblem {
+public static void main(String[] args) {
+		
+	    System.out.println("Welcome To The Line Comparision Computation");
+
+}
+}
